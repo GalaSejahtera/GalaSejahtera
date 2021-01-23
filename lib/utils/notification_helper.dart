e03afd6fb03f3c2130cca92ba91713f5035614ec
weinghwee <weinghwee.wee@hotmail.com>
2021-01-23 15:20:14 +0800

    fixed notification logo

--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -27,7 +27,7 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
 const AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('logo_my_sejahtera');
+    AndroidInitializationSettings('logo');
 
 final IOSInitializationSettings initializationSettingsIOS =
     IOSInitializationSettings(onDidReceiveLocalNotification:
